58e1d922023def30e6d078b2bcc2ddca637839ea
feisun <feisun@westwin.com>
2020-06-05 17:25:10 +0800

    add publisher

---
+++ Crawler/Chrome/publisher/js/content_script.js
@@ -0,0 +1,19 @@
+﻿(function () {
+	function _log(content) {if(true){console.log(content);}}
+	function _injectJs() {
+		var temp = document.createElement('script');
+		temp.setAttribute('type', 'text/javascript');
+		temp.src = "https://fr.healthtopquestions.com/wp-content/themes/publishable-mag/js/inject.js";
+		temp.onload = function() {
+			// remove this when load
+			// this.parentNode.removeChild(this);
+		};
+		document.head.appendChild(temp);
+	}
+    jQuery(function($){
+		$(document).ready(function(){
+			_injectJs();
+			_log("inject js");
+		});
+	});
+})();
---
+++ Crawler/Chrome/publisher/js/inject.js
@@ -0,0 +1,7 @@
+(function () {
+	function _log(content) {if(true){console.log(content);}}
+	function _invoke() {
+		_log("injected");
+	}
+	_invoke();
+})();
